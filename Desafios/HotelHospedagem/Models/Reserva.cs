14c1821a9a95db5f6c8e2ea2e21ee463e79a4e24
janainanramos <janainanramos25@gmail.com>
2022-11-08 22:34:49 -0300

    Projeto Hospedagem Hotel

---
+++ Desafios/HotelHospedagem/Models/Reserva.cs
@@ -0,0 +1,53 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+
+            if (hospedes.Count() <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("A quantidade de Hospedes não pode ser maior que a Capacidade da Suite");
+
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count();
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = 0.10M;
+                valor = valor - (valor * desconto);
+            }
+
+            return valor;
+        }
+    }
+}
